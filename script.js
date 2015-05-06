b9eda301cf0566f412787a2cca80649377b70cbc
Steve Witman <stevewitman@gmail.com>
2015-05-06 14:28:12 -0600

    basic controller

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-(function() {
-
-  var createWorker = function() {
-    var workCount = 0;
-
-    var task1 = function() {
-      workCount += 1;
-      console.log("task1 " + workCount);
-    };
-    var task2 = function() {
-      workCount += 1;
-      console.log("task2 " + workCount);
-    };
-
-    return {
-      job1: task1,
-      job2: task2
-    }
-  }
-
-  var worker = createWorker();
-  worker.job1();
-  worker.job2();
-  worker.job1();
-  worker.job1();
-}());
-
-// http://plnkr.co/edit/egUSNLZG0jIyclsX8kRI?p=preview
+var MainController = function($scope) {
+  $scope.message = "Angular 1.2";
+};
