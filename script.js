acd82d3682a73f16107ce08563c5954afdb1d934
Steve Witman <stevewitman@gmail.com>
2015-05-06 14:47:36 -0600

    attached model information to  object

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 var MainController = function($scope) {
+  var person = {
+    firstName: "Scott",
+    lastName: "Allen",
+    imageSrc: "http://stevewitman.com/img/preview-small.jpg"
+  }
+
   $scope.message = "Angular 1.2";
+  $scope.person = person;
 };
