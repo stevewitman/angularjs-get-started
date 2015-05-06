2a2934ee4bc24299d1eeb8d01b7bbf0c49813640
Steve Witman <stevewitman@gmail.com>
2015-05-06 14:09:46 -0600

    use IIFE to control scope of variables

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-var createWorker = function() {
-  var workCount = 0;
+(function() {
 
-  var task1 = function(){
-    workCount += 1;
-    console.log("task1 " + workCount);
-  };
-  var task2 = function(){
-    workCount += 1;
-    console.log("task2 " + workCount);
-  };
+  var createWorker = function() {
+    var workCount = 0;
 
-  return {
-    job1: task1,
-    job2: task2
+    var task1 = function() {
+      workCount += 1;
+      console.log("task1 " + workCount);
+    };
+    var task2 = function() {
+      workCount += 1;
+      console.log("task2 " + workCount);
+    };
+
+    return {
+      job1: task1,
+      job2: task2
+    }
   }
-}
 
-var worker = createWorker();
-
-worker.job1();
-worker.job2();
-worker.job1();
-worker.job1();
+  var worker = createWorker();
+  worker.job1();
+  worker.job2();
+  worker.job1();
+  worker.job1();
+}());
 
 // http://plnkr.co/edit/egUSNLZG0jIyclsX8kRI?p=preview
